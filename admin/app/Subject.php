5724bcbea65a79b4c347242f9da797814633947e
Fajar Jr <fajarjunyor13@gmail.com>
2019-07-11 16:54:58 +0700

    make model with crm for article and subject

---
+++ admin/app/Subject.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    protected $table = "subject";
+    protected $fillable = [
+        'name', 'description',
+    ];
+}
---
+++ admin/database/migrations/2019_07_11_095103_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('img_url', 500);
+            $table->string('text_content', 1500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
