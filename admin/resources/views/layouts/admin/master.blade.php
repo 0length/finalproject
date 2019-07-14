93d5f2f70ed9a7da408e176bd60f20521ad1199d
Fajar Jr <fajarjunyor13@gmail.com>
2019-07-14 17:04:17 +0700

    .

--- admin/resources/views/layouts/admin/master.blade.php
+++ admin/resources/views/layouts/admin/master.blade.php
@@ -17,19 +17,6 @@
 <meta name="apple-mobile-web-app-status-bar-style" content="default">
 <meta content="" name="description" />
 <meta content="" name="author" />
-<link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" />
-<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-<link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
-<link href="assets/plugins/jquery-scrollbar/jquery.scrollbar.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="assets/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="assets/plugins/switchery/css/switchery.min.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="assets/plugins/jquery-datatable/media/css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css" />
-<link href="assets/plugins/jquery-datatable/extensions/FixedColumns/css/dataTables.fixedColumns.min.css" rel="stylesheet" type="text/css" />
-<link href="assets/plugins/datatables-responsive/css/datatables.responsive.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="pages/css/pages-icons.css" rel="stylesheet" type="text/css">
-
-
-<link class="main-stylesheet" href="pages/css/pages.css" rel="stylesheet" type="text/css" />
 
 @yield('headerlib')
 </head>
