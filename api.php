eebb087027148507c9b30505fdb4077fad40ecb6
fajarTemp <fajarjunyor13@gmail.com>
2019-07-17 16:43:20 +0700

    turn off php notice and warning

--- api.php
+++ api.php
@@ -14,16 +14,16 @@ $table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
 $key = array_shift($request)+0;
  
 // escape the columns and values from the input object
-$columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
-$values = array_map(function ($value) use ($link) {
+$columns = preg_replace('/[^a-z0-9_]+/i','',@array_keys($input));
+$values = @array_map(function ($value) use ($link) {
   if ($value===null) return null;
   return mysqli_real_escape_string($link,(string)$value);
-},array_values($input));
+},@array_values($input));
  
 // build the SET part of the SQL command
 $set = '';
 for ($i=0;$i<count($columns);$i++) {
-  $set.=($i>0?',':'').'`'.$columns[$i].'`=';
+  @$set.=($i>0?',':'').'`'.$columns[$i].'`=';
   $set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
 }
  
