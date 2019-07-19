4b225fbd50ed17918ee4842259c2484c0db17928
Fajar Jr <fajarjunyor13@gmail.com>
2019-07-19 13:51:20 +0700

    update field subject

--- admin/database/migrations/2019_07_11_094607_create_subjects_table.php
+++ admin/database/migrations/2019_07_11_094607_create_subjects_table.php
@@ -16,6 +16,7 @@ class CreateSubjectsTable extends Migration
         Schema::create('subjects', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 20);
+            $table->string('img_url', 500);
             $table->string('description', 100);
             $table->timestamps();
         });
